2ee8542778f22b4cb67ce85fe7218ecde68bf5de
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2024-05-02 09:55:53 -0300

    Package Json and Imports Updated

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 export default function RootLayout({
   children,
 }: {
