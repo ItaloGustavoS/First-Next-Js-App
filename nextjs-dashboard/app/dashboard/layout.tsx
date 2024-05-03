57f9174e132175f55affa67b44bb61a1d308de87
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2024-05-03 09:48:29 -0300

    Imported Analytics and Speed Components

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,10 +1,14 @@
 import SideNav from '@/app/dashboard/sidenav';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
+        <Analytics />
+        <SpeedInsights />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
